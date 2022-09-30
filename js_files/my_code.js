ff7d16e6077661c00870575f1ec584c96dee37c3
baigsyboy <79266977+baigsyboy@users.noreply.github.com>
2022-09-30 12:47:35 +0100

    Add files via upload

--- js_files/my_code.js
+++ js_files/my_code.js
@@ -1,1 +1,56 @@
+// Check local storage for parameters or get from URL
+// This is to deal with the redirect
+if (localStorage.conversationId){
+   sendDTMF;
+} else{
+  const urlParams = new URLSearchParams(window.location.search); 
+  localStorage.setItem('conversationId', urlParams.get('conv'));
+  localStorage.setItem('participantId', urlParams.get('part'));
+  localStorage.setItem('DTMFdigits', urlParams.get('digits'));
+  console.log("Conv: ", localStorage.getItem('conversationId'), "Part: ", localStorage.getItem('participantId'), "Digits: ", localStorage.getItem('DTMFdigits'));
+}
 
+// Obtain a reference to the platformClient object
+const platformClient = require('platformClient');
+
+// Sets the environment to Dublin
+
+const client = platformClient.ApiClient.instance;
+client.setEnvironment("mypurecloud.ie");
+
+// Keep using the same token
+client.setPersistSettings(true);
+
+
+// Authenticate then update the presence on the page
+//client.loginImplicitGrant("10b85dde-54ad-4bb1-b0b0-e80c29b39e4e", "https://baigsyboy.github.io/GenCloud-change-status/", { state: "TEST" })
+//  .then(() => {
+//    console.log('Logged-In');
+//  })
+//  .catch((err) => {
+    // Handle failure response
+//    console.log(err);
+//  });
+
+function sendDTMF(){
+  let apiInstance = new platformClient.ConversationsApi();
+
+  const StrConversationId = localStorage.getItem('conversationId');
+  const StrParticipantId = localStorage.getItem('participantId');
+  const StrDigits = localStorage.getItem('DTMFdigits');
+  localStorage.clear();
+  
+  let opts = { 
+    'body': {"digits": StrDigits} // Object | Digits
+  };
+  
+  apiInstance.postConversationParticipantDigits(StrConversationId, StrParticipantId, opts)
+    .then(() => {
+      console.log('postConversationParticipantDigits returned successfully.');
+    })
+    .catch((err) => {
+      console.log('There was a failure calling postConversationParticipantDigits');
+      console.error(err);
+    });
+
+}
