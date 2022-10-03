49439e21a87703aff7afacc8abd3089e614bc2ab
baigsyboy <79266977+baigsyboy@users.noreply.github.com>
2022-10-03 11:30:20 +0100

    Add files via upload

--- js_files/my_code.js
+++ js_files/my_code.js
@@ -48,7 +48,7 @@ client.setEnvironment("mypurecloud.ie");
 client.setPersistSettings(true);
 
 
-// Authenticate then update the presence on the page
+// Authenticate then send the DTMF
 client.loginImplicitGrant("10b85dde-54ad-4bb1-b0b0-e80c29b39e4e", "https://baigsyboy.github.io/GenCloud-SendDTMF/", { state: "TEST" })
   .then(() => {
     console.log('Logged In');
